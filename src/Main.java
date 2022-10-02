0f07b72fbf267613f0fe4baa3a8c9c9582ccd0a5
Alex <seredin-alex@mail.ru>
2022-10-02 15:44:36 +0300

    Задание 1 выполнено

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) { task1(); }
+    public static void task1() {
+        System.out.println("Урок 6. Задание 1");
+        int clientOs = 1;
+
+        if (clientOs == 0) {
+            System.out.println("Установите приложение для ios по ссылке");
+        } else {
+            System.out.println("Установите приложение для Андроид по ссылке");
+        }
+    }
+}
